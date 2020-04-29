bb6b73fdd93c22e24b82c8be29fb0c4471fbe5ef
wasim919 <wasimwik919@gmail.com>
2020-04-29 16:16:15 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $(".materialboxed").materialbox();
+  $(".parallax").parallax();
+  $(".scrollspy").scrollSpy();
+  $(".fixed-action-btn").floatingActionButton();
+  $(".collapsible").collapsible();
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $("nav").addClass("sticky-nav");
+    } else {
+      $("nav").removeClass("sticky-nav");
+    }
+    if ($(window).scrollTop() > 150) {
+      $("sidenav").addClass("sticky-nav");
+    } else {
+      $("sidenav").removeClass("sticky-nav");
+    }
+  });
+});
+
+var link = document.getElementById("send_to_mail");
+link.onclick = function () {
+  this.href =
+    "mailto:wasimishaqkhan919@gmail.com?subject=Hello%20From%20Web&body=";
+  this.href += getBody();
+};
+
+function getBody() {
+  return document.getElementById("message").value;
+}
