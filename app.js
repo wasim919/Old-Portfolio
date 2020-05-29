2ae4c678baf7785e0dec13f89e038fbe334b6c8d
wasim919 <wasimwik919@gmail.com>
2020-05-30 01:11:59 +0530

    Carousel done

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 $(document).ready(function () {
-  $(".sidenav").sidenav();
-  $(".materialboxed").materialbox();
-  $(".parallax").parallax();
-  $(".scrollspy").scrollSpy();
-  $(".fixed-action-btn").floatingActionButton();
-  $(".collapsible").collapsible();
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.collapsible').collapsible();
+  $('.carousel').carousel({ numVisible: 6 });
 
   $(window).scroll(function () {
     if ($(window).scrollTop() > 100) {
-      $("nav").addClass("sticky-nav");
+      $('nav').addClass('sticky-nav');
     } else {
-      $("nav").removeClass("sticky-nav");
+      $('nav').removeClass('sticky-nav');
     }
     if ($(window).scrollTop() > 100) {
-      $("sidenav").addClass("sticky-nav");
+      $('sidenav').addClass('sticky-nav');
     } else {
-      $("sidenav").removeClass("sticky-nav");
+      $('sidenav').removeClass('sticky-nav');
     }
   });
 });
 
-var link = document.getElementById("send_to_mail");
+var link = document.getElementById('send_to_mail');
 link.onclick = function () {
   this.href =
-    "mailto:wasimishaqkhan919@gmail.com?subject=Hello%20From%20Web&body=";
+    'mailto:wasimishaqkhan919@gmail.com?subject=Hello%20From%20Web&body=';
   this.href += getBody();
 };
 
 function getBody() {
-  return document.getElementById("message").value;
+  return document.getElementById('message').value;
 }
